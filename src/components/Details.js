e3afbc3f8740e40a1a0427884549513619654d0f
INVALID <invalid@localhost.localdomain>
2020-04-04 19:42:03 +0300

    first react app commit

---
+++ src/components/Details.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+import {ProductConsumer} from '../context'
+import {Link} from 'react-router-dom'
+import {ButtonContainer} from './Button'
+
+export default class Details extends Component {
+    render() {
+        return (
+            <ProductConsumer>
+                {(value)=>{
+                    const {id, company, img, info, price, title, inCart} = value.detailProduct;
+                    return (
+                        <div className="container py-5">
+                            {/* title */}
+                            <div className="row">
+                                <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                                    <h1>{title}</h1>
+                                    {/* product info */}
+                                    <div className="row">
+                                        {/* product image */}
+                                        <div className="col-10 mx-auto col-md-6 my-3 ">
+                                            <img src={img} className="img-fluid" alt={title}/>
+                                        </div>
+                                        {/* end of product image */}
+                                        {/* product text */}
+                                        <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                                            <h3>model : {title}</h3>
+                                            <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                                                made by : <span className="text-uppercase">
+                                                    {company}
+                                                </span>
+                                            </h4>
+                                            <h4 className="text-blue">
+                                                <strong>
+                                                    price : <span>$</span>
+                                                    {price}
+                                                </strong>
+                                            </h4>
+                                            <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                                                some info about the product
+                                            </p>
+                                            <p className="text-muted lead">
+                                                {info}
+                                            </p>
+                                            {/* buttons */}
+                                            <div>
+                                                <Link  to="/">
+                                                    <ButtonContainer>
+                                                        back to product
+                                                    </ButtonContainer>
+                                                </Link>
+                                                <ButtonContainer 
+                                                cart
+                                                disabled={inCart ? true: false}
+                                                onClick={()=>{
+                                                    value.addToCart(id);
+                                                    value.openModal(id);
+                                                }}
+                                                >
+                                                    {inCart ? 'in Cart' : 'Add to cart'}
+                                                </ButtonContainer>
+                                            </div>
+                                            {/* end of buttons */}
+                                        </div>
+                                        {/* end of product text */}
+                                    </div>
+                                    {/*end of product info */}
+
+                                </div>
+                            </div>
+                            {/*end of  title */}
+                        </div>
+                    )
+                    
+                }}
+            </ProductConsumer>
+        )
+    }
+}
